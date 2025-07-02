ec81533c78be5db0b224a8884556ab3537aa89f9
WilsonHuang1 <wilson.at.work0110@gmail.com>
2025-07-02 09:23:07 +0800

    add build

---
+++ deptrum-stream-aurora900-windows-amd64-v1.1.6/build/src/cmake_install.cmake
@@ -0,0 +1,54 @@
+# Install script for directory: C:/Users/31110/Downloads/deptrum-stream-aurora900-windows-amd64-v1.1.6/samples/src
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/samples")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/31110/Downloads/deptrum-stream-aurora900-windows-amd64-v1.1.6/samples/build/src/sample_firmware_upgrade/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/31110/Downloads/deptrum-stream-aurora900-windows-amd64-v1.1.6/samples/build/src/sample_hello_deptrum/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/31110/Downloads/deptrum-stream-aurora900-windows-amd64-v1.1.6/samples/build/src/sample_viewer/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/31110/Downloads/deptrum-stream-aurora900-windows-amd64-v1.1.6/samples/build/src/sample_lite/cmake_install.cmake")
+endif()
+
---
+++ deptrum-stream-aurora900-windows-amd64-v1.1.6/build/src/sample_lite/cmake_install.cmake
@@ -0,0 +1,46 @@
+# Install script for directory: C:/Users/31110/Downloads/deptrum-stream-aurora900-windows-amd64-v1.1.6/samples/src/sample_lite
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/samples")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/samples/bin" TYPE EXECUTABLE FILES "C:/Users/31110/Downloads/deptrum-stream-aurora900-windows-amd64-v1.1.6/samples/bin/Debug/sample_lite.exe")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/samples/bin" TYPE EXECUTABLE FILES "C:/Users/31110/Downloads/deptrum-stream-aurora900-windows-amd64-v1.1.6/samples/bin/Release/sample_lite.exe")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/samples/bin" TYPE EXECUTABLE FILES "C:/Users/31110/Downloads/deptrum-stream-aurora900-windows-amd64-v1.1.6/samples/bin/MinSizeRel/sample_lite.exe")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/samples/bin" TYPE EXECUTABLE FILES "C:/Users/31110/Downloads/deptrum-stream-aurora900-windows-amd64-v1.1.6/samples/bin/RelWithDebInfo/sample_lite.exe")
+  endif()
+endif()
+
